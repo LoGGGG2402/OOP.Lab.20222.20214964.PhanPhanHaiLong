49d5eb129f3be102177d90a5fb3b0af62cbbb8ff
LOGGGG2402 <long.pph214964@sis.hust.edu.vn>
2023-05-12 08:58:10 +0700

    lab03

--- garbage/GarbageCreator.java
+++ garbage/GarbageCreator.java
@@ -1,2 +1,22 @@
-package garbage;public class GarbageCreator {
+package garbage;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class GarbageCreator {
+    public static void createGarbage() {
+        try {
+            File file = new File("longfile.txt");
+            FileInputStream inputStream = new FileInputStream(file);
+            byte[] buffer = new byte[1024];
+            String result = "";
+            while (inputStream.read(buffer) != -1) {
+                result += new String(buffer);
+            }
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
