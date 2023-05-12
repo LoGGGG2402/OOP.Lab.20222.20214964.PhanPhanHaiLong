38bbbf990cd5c3e2f27f07a72a507d91d3765f9e
LOGGGG2402 <long.pph214964@sis.hust.edu.vn>
2023-05-12 09:16:20 +0700

    in class

---
+++ OtherProjects/Lab01/NumberStar.java
@@ -0,0 +1,20 @@
+package OtherProjects.Lab01;
+
+import java.util.Scanner;
+public class NumberStar {
+    public static void main(String args[]){
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = keyboard.nextInt();
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= number-i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2*i-1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
---
+++ OtherProjects/Lab01/TwoDouble.java
@@ -0,0 +1,14 @@
+package OtherProjects.Lab01;
+
+import javax.swing.JOptionPane;
+public class TwoDouble {
+    public static void main(String[] args){
+        String input = JOptionPane.showInputDialog("Enter the first number: ");
+        double firstNumber = Double.parseDouble(input);
+        input = JOptionPane.showInputDialog("Enter the second number: ");
+        double secondNumber = Double.parseDouble(input);
+        double sum = firstNumber + secondNumber;
+        JOptionPane.showMessageDialog(null, "The sum of " + firstNumber + " and " + secondNumber + " is " + sum);
+        System.exit(0);
+    }
+}
