9c598eccbc75cdcdf184b875c103834752b4541a
LOGGGG2402 <long.pph214964@sis.hust.edu.vn>
2023-05-26 16:38:19 +0700

    lab04

---
+++ OtherProjects/hust/soict/dsai/lab04/hust/soict/dsai/aims/media/Disc.java
@@ -0,0 +1,60 @@
+package OtherProjects.hust.soict.dsai.lab04.hust.soict.dsai.aims.media;
+
+public abstract class Disc extends Media implements Playable{
+    int length;
+    String director;
+
+    public int getLength() {
+        return length;
+    }
+    public String getDirector() {
+        return director;
+    }
+
+    public Disc() {
+        super();
+    }
+
+    public Disc(String title) {
+        setTitle(title);
+    }
+
+    public Disc(String category, String title) {
+        setTitle(title);
+        setCategory(category);
+    }
+
+    public Disc(String director, String category, String title) {
+        setTitle(title);
+        setCategory(category);
+        this.director = director;
+    }
+
+    public Disc(String director, String category, String title, int length) {
+        setTitle(title);
+        setCategory(category);
+        this.director = director;
+    }
+
+    public Disc(String director, String category, String title, int length, float cost) {
+        setTitle(title);
+        setCategory(category);
+        this.director = director;
+        this.length = length;
+        setCost(cost);
+    }
+
+    @Override
+    public String toString() {
+        return "Disc{" +
+                "id=" + getId() +
+                ", title='" + getTitle() + '\'' +
+                ", category='" + getCategory() + '\'' +
+                ", cost=" + getCost() +
+                ", length=" + length +
+                ", director='" + director + '\'' +
+                '}';
+    }
+
+
+}
